adabe82f22429c1e1d0b7b967e87cb8e28bd1045
McMahon <stephanie.p.mcmahon@gmail.com>
2022-02-25 17:16:41 -0500

    garage cleaning
    cleaned up garage columns in Steph subfolder and added the code to the main cleaning file

---
+++ Stephanie/cleaning.py
@@ -0,0 +1,45 @@
+import pandas as pd 
+import numpy as np 
+pd.options.display.max_rows = 100
+
+HousePriceDF = pd.read_csv("Ames_HousePrice.csv")
+
+
+HousePriceDF=HousePriceDF.drop(['Alley','Fence','MiscFeature','PoolQC'],axis =1 )
+
+HousePriceDF['LotFrontage'] = HousePriceDF['LotFrontage'].fillna(HousePriceDF.groupby('Neighborhood')['LotFrontage'].transform('mean'))
+
+
+
+# Working on cleaning garage catagories: 
+
+#garage sub DF for exmaning
+garage = HousePriceDF.filter(regex='^Garage',axis=1)
+
+garage.isna().sum() 
+
+
+garage.loc[(garage["GarageType"].notnull()) & garage["GarageYrBlt"].isna()]
+
+# want to look at rows 433 and 531
+
+HousePriceDF['GarageType'].loc[433] = "None"
+
+
+
+HousePriceDF["GarageYrBlt"].loc[531] = 1983
+
+HousePriceDF["GarageFinish"].loc[531] = "Unf"
+
+HousePriceDF["GarageQual"].loc[531] = "TA"
+
+HousePriceDF["GarageCond"].loc[531] = "TA"
+
+HousePriceDF.fillna('None', inplace=True)
+
+
+
+
+
+
+
--- data_cleaning.py
+++ data_cleaning.py
@@ -5,13 +5,18 @@ pd.options.display.max_rows = 100
 HousePriceDF = pd.read_csv("Aimes-Iowa-ML\\Ames_HousePrice.csv")
 
 
+# drop columns with over 98% missing values 
 HousePriceDF=HousePriceDF.drop(['Alley','Fence','MiscFeature','PoolQC'],axis =1 )
 
+# impute frontage based on mean for the neighborhood 
 HousePriceDF['LotFrontage'] = HousePriceDF['LotFrontage'].fillna(HousePriceDF.groupby('Neighborhood')['LotFrontage'].transform('mean'))
 
+
+# change FireplaceQu to 'None' for non existent fireplaces 
 HousePriceDF.loc[HousePriceDF["Fireplaces"] == 0, "FireplaceQu"] = 'None'
 
 
+# cleaning up the Basement columns 
 HousePriceDF['BsmtQual'][HousePriceDF.Foundation == 'Slab'] = "None"
 HousePriceDF['BsmtExposure'][HousePriceDF.Foundation == 'Slab'] = "None"
 HousePriceDF['BsmtCond'][HousePriceDF.Foundation == 'Slab'] = "None"
@@ -26,6 +31,23 @@ HousePriceDF.iloc[912,37] = 'None'
 HousePriceDF.iloc[912,36] = 0
 HousePriceDF.iloc[912,36] = 0
 HousePriceDF.iloc[912,38:41] = 0 
+
+
+# cleaning up the Garage columns 
+HousePriceDF['GarageType'].loc[433] = "None"
+
+HousePriceDF["GarageYrBlt"].loc[531] = 1983
+
+HousePriceDF["GarageFinish"].loc[531] = "Unf"
+
+HousePriceDF["GarageQual"].loc[531] = "TA"
+
+HousePriceDF["GarageCond"].loc[531] = "TA"
+
+
+
+# DO NOT UNCOMMENT THIS UNTIL SURE WE ARE DONE IMPUTING 
+#HousePriceDF.fillna('None', inplace=True)
 
 
 
