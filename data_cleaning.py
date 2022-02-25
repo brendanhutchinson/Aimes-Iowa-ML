e1965ba92ecda82e225285fac95c6b6a4032d275
Brendanhutchinson <brendan.s.hutchinson@gmail.com>
2022-02-25 16:08:31 -0500

    cleaning basement columns

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,5 +9,23 @@ HousePriceDF=HousePriceDF.drop(['Alley','Fence','MiscFeature','PoolQC'],axis =1 
 
 HousePriceDF['LotFrontage'] = HousePriceDF['LotFrontage'].fillna(HousePriceDF.groupby('Neighborhood')['LotFrontage'].transform('mean'))
 
-HousePriceDF.loc[HousePriceDF["Fireplaces"] == 0, "FireplaceQu"] = np.nan
+HousePriceDF.loc[HousePriceDF["Fireplaces"] == 0, "FireplaceQu"] = 'None'
+
+
+HousePriceDF['BsmtQual'][HousePriceDF.Foundation == 'Slab'] = "None"
+HousePriceDF['BsmtExposure'][HousePriceDF.Foundation == 'Slab'] = "None"
+HousePriceDF['BsmtCond'][HousePriceDF.Foundation == 'Slab'] = "None"
+HousePriceDF['BsmtFinType1'][HousePriceDF.Foundation == 'Slab'] = "None"
+HousePriceDF['BsmtFinType2'][HousePriceDF.Foundation == 'Slab'] = "None"
+HousePriceDF.loc[(HousePriceDF['BsmtFinSF1']== 0),32:36] = "None"
+HousePriceDF.iloc[2434,37] = 'Unf'
+HousePriceDF.iloc[912,32:36] = "None"
+HousePriceDF.iloc[912,32:36] = "None"
+HousePriceDF.loc[(HousePriceDF['BsmtFinSF1']== 0),'BsmtFinType2' ] = "None"
+HousePriceDF.iloc[912,37] = 'None'
+HousePriceDF.iloc[912,36] = 0
+HousePriceDF.iloc[912,36] = 0
+HousePriceDF.iloc[912,38:41] = 0 
+
+
 
