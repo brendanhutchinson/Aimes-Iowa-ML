ef881c07e073c2f45d70689c8abca7cbdc92de9a
Brendanhutchinson <brendan.s.hutchinson@gmail.com>
2022-03-09 00:56:19 -0500

    Grid Search, Feature Importance, model evaluation

--- CatBoost.py
+++ CatBoost.py
@@ -1,8 +1,46 @@
 
-from trees import *
 
 from trees import *
 from catboost import CatBoostRegressor
+import pandas as pd 
+import numpy as np 
+
+
+
+# Selected features 
+
+numeric_1 = ['GrLivArea', 'LotFrontage','LotArea', 'Street', 'LotShape', 'LandContour', 'Utilities', 
+            'LandSlope', 'OverallQual','OverallCond' , 'YearBuilt', 'YearRemodAdd',
+           'MasVnrArea', 'ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 
+           'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF', 'HeatingQC', 
+             'Electrical', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'BsmtFullBath', 
+           'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'KitchenQual',
+           'TotRmsAbvGrd', 'Functional', 'Fireplaces', 'FireplaceQu', 'GarageFinish', 
+           'GarageCars', 'GarageArea', 'GarageQual',  'PavedDrive', 'WoodDeckSF', 
+           'EnclosedPorch',  'ScreenPorch', 'PoolArea', 'MiscVal' , 'MoSold', 'YrSold', 
+           'SaleCondition', 'YrSinceRm','Pool', 'Misc', 'BsmtFinTotSF', 'IAstateDist', 'AirportDist', 'TotalBath']
+
+categorical_1  = ['MSSubClass', 'MSZoning', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType',
+               'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st' , 'Exterior2nd', 'MasVnrType',
+               'Foundation', 'Heating', 'GarageType','SaleType' , 'LotConfig' ] 
+
+
+
+# dummifying variables 
+
+numeric = numeric_1
+
+categorical = categorical_1
+
+y = np.log(df['SalePrice'])
+
+# DO NOT TOUCH THIS CODE 
+dummies = pd.get_dummies(df[categorical], drop_first=True)
+
+X = pd.concat([df[numeric], dummies], axis = 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+
 
 # Cat Boost with defualt parameters 
 
@@ -21,21 +59,69 @@ parameters = {
                  
 
 
-grid_tree = GridSearchCV(model, param_grid= parameters, cv=3, n_jobs=-1).fit(X_train,y_train)
+grid_cat = GridSearchCV(model, param_grid= parameters, cv=5, n_jobs=-1).fit(X_train,y_train)
+
+parameters2 = {
+              'learning_rate': [.001,.01,.1,.5,1],
+              'max_depth': [3,4,6,10],
+               'n_estimators': [100,200,300,500,700,1000]}
+                 
+grid_tree = GridSearchCV(model, param_grid= parameters2, cv=5, n_jobs=-1).fit(X_train,y_train)
 
 
-print('Best grid parameters: '+ str(grid_tree.best_params_))
-print(grid_tree.best_score_)
-print(randomForest.oob_score_)
+
+
+
+print('optimal grid parameters: '+ str(grid_cat.best_params_))
+print(grid_cat.best_score_)
 
 
 # Scoring Test data set with tuned parameters 
 
 
-model = CatBoostRegressor()
-model.set_params(learning_rate = , max_depth= , n_estimators= )
-model.fit(X_train, y_train)
-preds = model.predict(X_test)
-model.score(X_test,y_test)
 
-                
+cat = CatBoostRegressor()
+cat.set_params(random_state = 42 ,learning_rate = .08, max_depth = 4 , n_estimators = 900)
+cat.fit(X_train,y_train)
+cat.score(X_test,y_test)
+
+
+
+
+cat.set_params(random_state = 42 ,learning_rate = 0.1, max_depth = 4 , n_estimators = 500)
+cat.fit(X_train,y_train)
+cat.score(X_test,y_test)
+
+
+# Best Cat boost score 
+
+
+cat.set_params(random_state = 42 ,learning_rate = 0.1, max_depth = 6 , n_estimators = 700)
+cat.fit(X_train,y_train)
+cat.score(X_test,y_test)               
+
+
+# Feature Importances 
+
+tree_best = grid_cat.best_estimator_
+feature_importance = list(zip(X_train.columns, tree_best.feature_importances_))
+dtype = [('feature', 'S10'), ('score', 'float')]
+feature_importance = np.array(feature_importance, dtype=dtype)
+sort_X = np.sort(feature_importance, order='score')[::-1]
+features, score = zip(*list(sort_X))
+featuredf = pd.DataFrame({'features':features,'score':score})
+
+
+
+featuredf.features =[str(i) for i in featuredf.features]
+featuredf.features =[i.replace('b','') for i in featuredf.features]
+featuredf.features =[i.replace('\'','') for i in featuredf.features]
+
+
+
+# scatterplot of Predicted vs Test target
+import math 
+y_pred = model.predict(X_test)
+y_test =[math.exp(i) for i in y_test]
+y_pred = [math.exp(i) for i in y_pred]
+px.scatter(x=y_test,y=y_pred,labels={'x':'Test Target ', 'y':'Model Predictions'})
--- trees.py
+++ trees.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
 
+from random import random
 from manipulation import * 
 from modeling import *
-
-
+from manipulation import * 
 from sklearn import ensemble
+import time
+time
+# Selected features 
+numeric_1 = ['GrLivArea', 'LotFrontage','LotArea', 'Street', 'LotShape', 'LandContour', 'Utilities', 
+            'LandSlope', 'OverallQual','OverallCond' , 'YearBuilt', 'YearRemodAdd',
+           'MasVnrArea', 'ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 
+           'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF', 'HeatingQC', 
+             'Electrical', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'BsmtFullBath', 
+           'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'KitchenQual',
+           'TotRmsAbvGrd', 'Functional', 'Fireplaces', 'FireplaceQu', 'GarageFinish', 
+           'GarageCars', 'GarageArea', 'GarageQual',  'PavedDrive', 'WoodDeckSF', 
+           'EnclosedPorch',  'ScreenPorch', 'PoolArea', 'MiscVal' , 'MoSold', 'YrSold', 
+           'SaleCondition', 'YrSinceRm','Pool', 'Misc', 'BsmtFinTotSF', 'IAstateDist', 'AirportDist', 'TotalBath']
+
+categorical_1  = ['MSSubClass', 'MSZoning', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType',
+               'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st' , 'Exterior2nd', 'MasVnrType',
+               'Foundation', 'Heating', 'GarageType','SaleType' , 'LotConfig' ] 
+
+
+
+# Random Forest 
+
 randomForest = ensemble.RandomForestClassifier()
 bagging = ensemble.BaggingClassifier()
-
-randomForest.set_params(random_state=42, n_estimators=100, max_features=20)
-randomForest.fit(X_train,y_train) 
-randomForest.score(X_train,y_train)
-randomForest.score(X_test,y_test)
-
 
 
 
 
 # input which subset of numeric & categorical variables to be used 
-numeric = num_cboost
 
-categorical = cat_cboost
+
+numeric = numeric_1
+
+categorical = categorical_1
 
 
 # DO NOT TOUCH THIS CODE 
@@ -31,23 +49,33 @@ X = pd.concat([df[numeric], dummies], axis = 1)
 # if subsetting use the code below before running tts
 # X = X[subset]
 
+#  split data for training and testing 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
 
 
-from manipulation import * 
+
 
 
 from sklearn import ensemble
 randomForest = ensemble.RandomForestRegressor()
 bagging = ensemble.BaggingRegressor()
+# Random Forest Default parameters 
 
-randomForest.set_params(random_state=42, n_estimators=100, max_features=20)
-randomForest.fit(X_train,y_train) 
+
+randomForest.fit(X_train,y_train)
 randomForest.score(X_train,y_train)
 randomForest.score(X_test,y_test)
 
+# baggin default parameters 
+
+bagging.fit(X_train, y_train)
+bagging.score(X_train,y_train)
+bagging.score(X_test,y_test)
+
+
 # bagging
+
 bagging.set_params(n_estimators=50, random_state=42, max_features=20)
 bagging.fit(X_train, y_train)
 print("The training error is: %.5f" %(1 - bagging.score(X_train, y_train)))
@@ -55,7 +83,24 @@ print("The test error is: %.5f" %(1 - bagging.score(X_test, y_test)))
 print("The training accuracy is: %.5f" %( bagging.score(X_train, y_train)))
 print("The test accuracy is: %.5f" %(bagging.score(X_test, y_test)))
 
+
+
+
+
+
 # Random Forest
+
+randomForest.set_params(random_state=42, n_estimators=100, max_features=20)
+randomForest.fit(X_train,y_train) 
+randomForest.score(X_train,y_train)
+randomForest.score(X_test,y_test)
+
+randomForest.set_params(random_state=42, n_estimators=100, max_features=5)
+randomForest.fit(X_train,y_train) 
+randomForest.score(X_train,y_train)
+randomForest.score(X_test,y_test)
+
+
 randomForest.set_params(n_estimators=50, random_state=42, max_features=10,oob_score=True)
 randomForest.fit(X_train, y_train)
 print("The training error  is: %.5f" %(1 - randomForest.score(X_train, y_train)))
@@ -66,8 +111,10 @@ print("The test accuracy is: %.5f" %(randomForest.score(X_test, y_test)))
 
 
 # Grid Search 
+
+
 randomForest.set_params(oob_score=True)
-grid_para_tree = {
+grid_tree = {
     'max_depth': range(1, 30),
     'n_estimators': range(10, 110, 10),
     'min_samples_split': range(2,10,2)
@@ -75,8 +122,8 @@ grid_para_tree = {
 }
 
 randomForest.set_params(random_state=0)
-grid_search_tree = GridSearchCV(randomForest.fit(X_train, y_train),grid_para_tree, scoring='r2', cv=5, n_jobs=-1, return_train_score=True)
-%time grid_search_tree.fit(X_train, y_train)
+grid_search_tree = GridSearchCV(randomForest,grid_tree, cv=5, n_jobs=-1, return_train_score=True)
+grid_search_tree.fit(X_train, y_train)
 
 
 print('Best parameters: '+ str(grid_search_tree.best_params_))
@@ -85,10 +132,27 @@ print(randomForest.oob_score_)
 
 
 
+grid_tree2 = {
+    'max_depth': range(1, 20),
+    'n_estimators': range(10, 1000, 50),
+    'max_features' : [10,30,50,70],
+}
+
+
+
+randomForest.set_params(random_state=42)
+grid_search_tree = ms.GridSearchCV(randomForest,grid_tree2, cv=5, n_jobs=-1)
+ 
+
+grid_search_tree.fit(X_train, y_train)
+
+
+print('Best parameters: '+ str(grid_search_tree.best_params_))
+print(grid_search_tree.best_score_)
 
 # Scoring test data set with tuned forest parameters 
 
-randomForest.set_params(n_estimators=, random_state=42, max_features= ,oob_score=True)
+randomForest.set_params(n_estimators=50, random_state=42, max_features=50 ,oob_score=True)
 randomForest.fit(X_train, y_train)
 print("The training error  is: %.5f" %(1 - randomForest.score(X_train, y_train)))
 print("The test error is: %.5f" %(1 - randomForest.score(X_test, y_test)))
@@ -97,8 +161,23 @@ print("The test accuracy is: %.5f" %(randomForest.score(X_test, y_test)))
 
 
 
+randomForest.set_params(random_state=42, n_estimators=100, max_features=70)
+randomForest.fit(X_train,y_train) # fit 
+randomForest.score(X_train,y_train)
+print(randomForest.score(X_test,y_test))
 
+randomForest.set_params(random_state=42, n_estimators=100, max_features=30, max_depth = 14)
+randomForest.fit(X_train,y_train) 
+randomForest.score(X_train,y_train)
+print(randomForest.score(X_test,y_test))
 
+# Random Forest Best Score 
+
+randomForest.set_params(random_state=42, n_estimators=700, max_features=30, max_depth = 14)
+randomForest.fit(X_train,y_train) 
+print(randomForest.score(X_train,y_train))
+print(randomForest.score(X_test,y_test))
+print("The best  test accuracy is: %.5f" %(randomForest.score(X_test, y_test)))
 
 
 
@@ -106,11 +185,13 @@ print("The test accuracy is: %.5f" %(randomForest.score(X_test, y_test)))
 
 tree_final = grid_search_tree.best_estimator_
 feature_importance = list(zip(X_train.columns, tree_final.feature_importances_))
-dtype = [('feature', 'S10'), ('importance', 'float')]
+dtype = [('feature', 'S10'), ('score', 'float')]
 feature_importance = np.array(feature_importance, dtype=dtype)
-feature_sort = np.sort(feature_importance, order='importance')[::-1]
-name, score = zip(*list(feature_sort))
+sort_X = np.sort(feature_importance, order='score')[::-1]
+name, score = zip(*list(sort_X))
 pd.DataFrame({'feature':name,'score':score}).plot.barh(x='name', y='score')
+
+
 
 
 
